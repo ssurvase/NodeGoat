0ac91252cc816c0c6f12c7ad1eab655a35403236
Sachin Survase <sachin.s@capiot.com>
2018-01-11 12:51:33 +0530

    unit test should be failing

--- test/unit/demo.js
+++ test/unit/demo.js
@@ -3,7 +3,7 @@ var test = require("selenium-webdriver/testing");
 test.describe('Demo unit test', function() {
 
 	test.it('always failing test', function() {
-        //throw Error('FIXME');
+        throw Error('FIXME');
     });
 
 });
