6ed3885e8fb4c2acf7d32115d12ccf414e1a2b58
Sachin Survase <sachin.s@capiot.com>
2018-01-11 12:38:57 +0530

    Added demo unit test

---
+++ test/unit/demo.js
@@ -0,0 +1,10 @@
+var test = require("selenium-webdriver/testing");
+
+test.describe('Demo unit test', function() {
+
+	test.it('always failing test', function() {
+        //throw Error('FIXME');
+    });
+
+});
+
